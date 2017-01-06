29c8b1fa8a40e1bd892eee65fabf3d896872fb11
zhaoyongjie <18515381796@163.com>
2017-01-06 11:19:42 +0800

    Initial Commit

---
+++ XZQRCode_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZQRCode_OC
+//
+//  Created by MYKJ on 17/1/6.
+//  Copyright © 2017年 zhaoyongjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XZQRCode_OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZQRCode_OC
+//
+//  Created by MYKJ on 17/1/6.
+//  Copyright © 2017年 zhaoyongjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
